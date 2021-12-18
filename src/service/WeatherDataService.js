7db57c2e0e2edaa155c114dcca2f83dd479a62e7
abhishek-shigavan <abhishekshigavan1437@gmail.com>
2021-12-19 01:23:29 +0530

    Add: File contains API calls to fetch data

---
+++ src/service/WeatherDataService.js
@@ -0,0 +1,15 @@
+import axios from "axios";
+
+const BASE_URL = "https://api.openweathermap.org/data/2.5/find?";
+const API_KEY = "704cad17de7e40ab39cff1ff5eb057a7";
+
+export const getDataByCityName = async function(cityName) {
+    const response = await axios.get(`${BASE_URL}q=${cityName}&appid=${API_KEY}`);
+    return response;
+}
+
+export const getDataByLatLon = async function(latitude, longitude) {
+    const response = await axios.get(`https://openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&units=metric&appid=439d4b804bc8187953eb36d2a8c26a02`);
+    console.log(response);
+    return response;
+}
