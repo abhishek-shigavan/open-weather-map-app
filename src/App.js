d9c2d96a8c8024dd18b5f2b354731be2a1389983
abhishek-shigavan <abhishekshigavan1437@gmail.com>
2021-12-19 23:21:02 +0530

    Refactor: Routes added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import './App.css';
 import Home from './components/Home';
+import { Routes, Route} from 'react-router-dom';
+import CityWeatherData from './components/CityWeatherData';
 
 function App() {
   return (
     <div className="App">
-      <Home />
+      <Routes>
+        <Route exact path="/" element={<Home/>}></Route>
+        <Route path="/q:CityName&id:CityID" exact element={<CityWeatherData/>}></Route>
+      </Routes>
     </div>
   );
 }
